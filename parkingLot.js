86e76d2d31964b1bc664ae04a8b97322b4d98ea7
Jennferzhan <1213857673@qq.com>
2018-08-28 14:59:00 +0800

    second commit

--- parkingLot.js
+++ parkingLot.js
@@ -1,26 +1,43 @@
-function ParkingLotTest() {
+function ParkingBoy() {
 }
 var parkingLots = []
-ParkingLotTest.prototype.park = function(name) {
+function sortArray (parkingLots) {
+	for (var i = 0;i<parkingLots.length;i++) {
+		if (parkingLots[i].cars > 0) {
+			for (var j = 0;j<parkingLots.length-i-1;j++){
+				if (parkingLots[j].cars < parkingLots[j+1].cars) {
+					var newArray = parkingLots[j]
+					parkingLots[j] = parkingLots[j+1]
+					parkingLots[j+1] = newArray
+				}
+			}
+			console.log(parkingLots)
+			return parkingLots
+		}
+	}
+}
+ParkingBoy.prototype.park = function(name) {
+	sortArray(parkingLots)
 	for (var i = 0;i<parkingLots.length;i++) {
 		if (parkingLots[i].cars > 0) {
 			parkingLots[i].parkingLotsPlace.push(name)
 			parkingLots[i].cars -=1
+			var info = [true, parkingLots[i].parkingLotsName]
 			// alert(parkingLots[i].parkingLotsPlace)
-			return name + '在' + (i+1) + '号停车场停车成功'
+			return info
 		}
 	}
-	return name + '停车失败'
+	return false
 };
-ParkingLotTest.prototype.pickUp = function(name) {
+ParkingBoy.prototype.pickUp = function(name) {
 	for (var i = 0;i<parkingLots.length;i++) {
 		for(var j=0;j<parkingLots[i].parkingLotsPlace.length;j++)
 		if (parkingLots[i].parkingLotsPlace[j] === name) {
 			parkingLots[i].parkingLotsPlace.splice(j, 1)
 			console.log(parkingLots)
 			parkingLots[i].cars +=1
-			return name + '取车成功'
+			return true
 		}
 	}
-	return '取车失败'
+	return false
 };
--- parkingLotSpec.js
+++ parkingLotSpec.js
@@ -2,60 +2,67 @@
 * @Author: john
 * @Date:   2018-07-25 20:25:42
 * @Last Modified by:   Jennferzhan
-* @Last Modified time: 2018-08-21 15:43:00
+* @Last Modified time: 2018-08-28 14:51:04
 */
-describe("ParkingLotTest", function() {
-  var parkingLotTest;
-  // var song;
+describe("ParkingBoy", function() {
+  var parkingboy;
 
   beforeEach(function() {
-    parkingLotTest = new ParkingLotTest();
-    // song = new Song();
+    parkingboy = new ParkingBoy();
   });
 // 停车场1总容量3，停车场2总容量1
 // 小弟有一个停车场，未满
   it("should_return_true_when_input_停车_and_PThird", function() {
-  	parkingLots = [{cars:1,parkingLotsPlace:['PFirst','PSecond']}]
-    var data = parkingLotTest.park('PThird')
-    expect(data).toEqual('PThird在1号停车场停车成功');
+    parkingLots = [{parkingLotsName:1,cars:1,parkingLotsPlace:['PFirst','PSecond']}]
+    var data = parkingboy.park('PThird')
+    expect(data).toEqual([true, 1]);
   });
   // 小弟有一个停车场，满了
   it("should_return_true_when_input_停车_and_PForth", function() {
-  	parkingLots = [{cars:0,parkingLotsPlace:['PFirst','PSecond','PThird']}]
-    var data = parkingLotTest.park('PForth')
-    expect(data).toEqual('PForth停车失败');
+    parkingLots = [{parkingLotsName:1,cars:0,parkingLotsPlace:['PFirst','PSecond','PThird']}]
+    var data = parkingboy.park('PForth')
+    expect(data).toEqual(false);
   });
   // 小弟有一个停车场，车存在
   it("should_return_true_when_input_取车_and_PFirst", function() {
-    parkingLots = [{cars:3,parkingLotsPlace:['PFirst','PSecond','PThird']}]
-    var data = parkingLotTest.pickUp('PFirst')
-    expect(data).toEqual('PFirst取车成功');
+    parkingLots = [{parkingLotsName:1,cars:3,parkingLotsPlace:['PFirst','PSecond','PThird']}]
+    var data = parkingboy.pickUp('PFirst')
+    expect(data).toEqual(true);
   });
   // // 两个停车场，一个满了，一个未满
   it("should_return_true_when_input_停车_and_PForth", function() {
     parkingLots = [
-                    {cars:0,parkingLotsPlace:['PFirst','PSecond','PThird']},
-                    {cars:3,parkingLotsPlace:[]}
+                    {parkingLotsName:1,cars:0,parkingLotsPlace:['PFirst','PSecond','PThird']},
+                    {parkingLotsName:2,cars:3,parkingLotsPlace:[]}
                   ]
-    var data = parkingLotTest.park('PForth')
-    expect(data).toEqual('PForth在2号停车场停车成功');
+    var data = parkingboy.park('PForth')
+    expect(data).toEqual([true, 2]);
   });
   // // 小弟有两个停车场，都满了
   it("should_return_true_when_input_停车_and_PFifth", function() {
     parkingLots = [
-                    {cars:0,parkingLotsPlace:['PFirst','PSecond','PThird']},
-                    {cars:0,parkingLotsPlace:['PForth']}
+                    {parkingLotsName:1,cars:0,parkingLotsPlace:['PFirst','PSecond','PThird']},
+                    {parkingLotsName:2,cars:0,parkingLotsPlace:['PForth']}
                   ]
-    var data = parkingLotTest.park('PFifth')
-    expect(data).toEqual('PFifth停车失败');
+    var data = parkingboy.park('PFifth')
+    expect(data).toEqual(false);
   });
   // 小弟有两个停车场，两个未满，优先第一个
   it("should_return_true_when_input_停车_and_PForth", function() {
     parkingLots = [
-                    {cars:1,parkingLotsPlace:['PFirst','PSecond']},
-                    {cars:1,parkingLotsPlace:['PThird']}
+                    {parkingLotsName:1,cars:1,parkingLotsName:1,parkingLotsPlace:['PFirst','PSecond']},
+                    {parkingLotsName:2,cars:1,parkingLotsPlace:['PThird']}
                   ]
-    var data = parkingLotTest.park('PForth')
-    expect(data).toEqual('PForth在1号停车场停车成功');
+    var data = parkingboy.park([true, 1])
+    expect(data).toEqual([true, 1]);
+  });
+  // 两个停车场优先停空车位多的
+  it("should_return_true_when_input_停车_and_PForth", function() {
+    parkingLots = [
+                    {parkingLotsName:1,cars:2,parkingLotsPlace:['PFirst','PSecond']},
+                    {parkingLotsName:2,cars:5,parkingLotsPlace:['PThird']}
+                  ]
+    var data = parkingboy.park('PForth')
+    expect(data).toEqual([true, 2]);
   });
 });
